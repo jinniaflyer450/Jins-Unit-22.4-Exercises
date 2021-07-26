90aafc27b82cdf1343aeee21e870b34f6d1c286f
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-07-26 14:34:27 -0500

    finished medical center schema, DDL creation, and tests

---
+++ medical_center.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS medical_center;
+
+CREATE DATABASE medical_center;
+
+\c medical_center;
+
+CREATE TABLE doctors (id SERIAL PRIMARY KEY, given_name VARCHAR(15) NOT NULL, surname VARCHAR(20) NOT NULL, is_surgeon BOOLEAN NOT NULL);
+--Added data to patients table that is not in the drawn schema--gender, sex, and birthdate.
+CREATE TABLE patients (id SERIAL PRIMARY KEY, given_name VARCHAR(15) NOT NULL, surname VARCHAR(20) NOT NULL, 
+gender VARCHAR(10), sex VARCHAR(10) NOT NULL, birthdate DATE NOT NULL);
+CREATE TABLE doctors_patients (id SERIAL PRIMARY KEY, doctor_id INTEGER REFERENCES doctors(id) NOT NULL, 
+patient_id INTEGER REFERENCES patients(id) NOT NULL);
+CREATE TABLE specialties (id SERIAL PRIMARY KEY, name VARCHAR(20) NOT NULL);
+CREATE TABLE doctors_specialties (id SERIAL PRIMARY KEY, doctor_id INTEGER REFERENCES doctors(id) NOT NULL, 
+specialty_id INTEGER REFERENCES specialties(id) NOT NULL);
+--Added specialty id to diseases table (not in the drawn schema).
+CREATE TABLE diseases (id SERIAL PRIMARY KEY, name VARCHAR(30) NOT NULL, specialty_id INTEGER REFERENCES specialties(id) NOT NULL);
+CREATE TABLE patients_diseases (id SERIAL PRIMARY KEY, patient_id INTEGER REFERENCES patients(id) NOT NULL, 
+disease_id INTEGER REFERENCES diseases(id) NOT NULL);
+
+INSERT INTO doctors (given_name, surname, is_surgeon) VALUES ('Alice', 'Jackson', false), ('Max', 'Wu', true);
+INSERT INTO patients (given_name, surname, gender, sex, birthdate) VALUES ('Caleb', 'Wesley', 'male', 'male', '2000-04-08'),
+('Taylor', 'Wozniak', 'nonbinary', 'female', '1980-08-04');
+INSERT INTO doctors_patients (doctor_id, patient_id) VALUES (1, 2), (2, 2), (2, 1);
+INSERT INTO specialties (name) VALUES ('gynecology'), ('orthopedics'), ('gastroentology');
+INSERT INTO doctors_specialties (doctor_id, specialty_id) VALUES (1, 1), (2, 2), (2, 3);
+INSERT INTO diseases (name, specialty_id) VALUES ('ovarian cancer', 1), ('tibia fracture', 2);
+INSERT INTO patients_diseases(patient_id, disease_id) VALUES (2, 1), (2, 2), (1, 2);
